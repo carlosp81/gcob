8fabc92169b8035959e8c1c634851289ede4340e
zapt81 <dev-pro@cpimentelt.com>
2026-09-11 14:17:05 -0500

    build: allow clippy::len_without_is_empty in generated protobuf
    Keeps 'cargo clippy --all-targets -- -D warnings' clean without editing
    generated code.

--- build.rs
+++ build.rs
@@ -2,11 +2,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let proto_dir = "proto/api/v1";
 
     tonic_prost_build::configure()
+        .type_attribute(".", "#[allow(clippy::len_without_is_empty)]")
         .build_server(false)
         .build_client(true)
         .compile_protos(&["cln/node.proto"], &[proto_dir])?;
 
     tonic_prost_build::configure()
+        .type_attribute(".", "#[allow(clippy::len_without_is_empty)]")
         .compile_protos(&["cln/services.proto", "cln/events.proto"], &[proto_dir])?;
 
     println!("cargo:rerun-if-changed=proto/api/v1/cln/node.proto");
